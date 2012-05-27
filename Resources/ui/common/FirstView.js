2df7528e171f13d21bca33982afe250026ffba5b
Jorge Luis <varjotanet@gmail.com>
2012-05-26 21:53:16 -0300

    ve se presta

--- Resources/ui/common/FirstView.js
+++ Resources/ui/common/FirstView.js
@@ -6,16 +6,44 @@ function FirstView() {
 	//label using localization-ready strings from <app dir>/i18n/en/strings.xml
 	var label = Ti.UI.createLabel({
 		color:'#000000',
-		text:String.format(L('welcome'),'Titanium'),
+		top: 20,
+		left: 20,
+		text:'Stock',
 		height:'auto',
 		width:'auto'
 	});
 	self.add(label);
 	
-	//Add behavior for UI
-	label.addEventListener('click', function(e) {
-		alert(e.source.text);
+	var edit = Ti.UI.createTextField ({
+		color:'#000000',
+		top: 50,
+		left: 20,
+		height:'auto',
+		width: 150
 	});
+	self.add(edit);
+	
+	var botao = Ti.UI.createButton ({
+		title: 'Quote!',
+		top: 100,
+		left: 20,
+		height:'auto',
+		width: 'auto'
+	});
+	self.add(botao);
+	
+	
+	//Add behavior for UI
+	botao.addEventListener('click', searchYQL);
+	
+	function searchYQL() {
+		query = 'select * from yahoo.finance.quotes where symbol = "' + edit.value + '" limit 1';
+		
+		Titanium.Yahoo.yql(query, function(e){
+			var data = e.data;
+			alert(data.quotes.Name);
+		});
+	};
 	
 	return self;
 }
